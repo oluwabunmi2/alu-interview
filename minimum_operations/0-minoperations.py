9876768f3b6444fd5486d7c218f4b56e4ce10cab
oluwabunmi2 <o.adebanjo@alustudent.com>
2023-02-01 06:48:53 +0000

    miniop

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""minimum Operations"""
+
+
+def minOperations(n):
+    """Returns an integer
+    If n is  the impossible to achieve, return 0"""
+
+    if not n or n < 2:
+        return 0
+    numberOperations = 0
+    for time in range(2, n+1):
+        while(n % time == 0):
+            numberOperations += time
+            n = n / time
+    return(numberOperations)
